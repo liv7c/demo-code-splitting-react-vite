11432d1cab79247b4e08f0be99b8bf82a2761cfa
liv7c <livie7c@gmail.com>
2025-03-24 13:17:06 +0100

    feat(Home): add content home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,36 @@
+import {Link} from 'react-router'
+
 function Home() {
-  return <h1>Home</h1>
+  return (
+    <div className="space-y-3">
+      <h1 className="text-3xl">Home</h1>
+      <p className="lg mt-4">Welcome to our super cool application.</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe facilis
+        ullam, veniam placeat illo rerum eligendi sapiente ratione minima dicta
+        assumenda voluptates magni nihil a architecto pariatur molestias
+        dignissimos non?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe facilis
+        ullam, veniam placeat illo rerum eligendi sapiente ratione minima dicta
+        assumenda voluptates magni nihil a architecto pariatur molestias
+        dignissimos non?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe facilis
+        ullam, veniam placeat illo rerum eligendi sapiente ratione minima dicta
+        assumenda voluptates magni nihil a architecto pariatur molestias
+        dignissimos non?
+      </p>
+      <Link
+        to="/movies"
+        className="mt-4 inline-block rounded bg-teal-800 px-3 py-2 text-white"
+      >
+        Start exploring our movies
+      </Link>
+    </div>
+  )
 }
 
 export default Home

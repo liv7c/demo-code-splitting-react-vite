41c44cd2b026234225c85211cd98c213d1a38a1e
liv7c <livie7c@gmail.com>
2025-03-24 13:17:48 +0100

    feat(api): set up react router and movie api hook

---
+++ src/api/movies.ts
@@ -0,0 +1,10 @@
+import type {Movie} from '../types/movie'
+
+export const fetchMovies = async (): Promise<{results: Movie[]}> => {
+  const resp = await fetch('https://swapi.dev/api/films')
+  if (!resp.ok) {
+    throw new Error(`unable to retrieve movies - ${resp.status}`)
+  }
+
+  return resp.json()
+}

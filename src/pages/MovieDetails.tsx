44f34e9269b6b74760c39ef1a5612f704beef1fb
liv7c <livie7c@gmail.com>
2025-03-25 21:03:26 +0100

    feat: use loader with react query for MovieDetails page

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,37 +1,39 @@
-import {Link, useParams} from 'react-router'
-import {useMovie} from '../hooks/useMovies'
+import type {LoaderFunctionArgs} from 'react-router'
+import {Link, useLoaderData} from 'react-router'
 import {formatDate} from '../utils/date'
+import type {QueryClient} from '@tanstack/react-query'
+import {getMovieDetailsQuery} from '../api/movies'
+import type {Movie} from '../types/movie'
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const movieDetailsLoader =
+  (queryClient: QueryClient) =>
+  async ({params}: LoaderFunctionArgs) => {
+    const query = getMovieDetailsQuery(params.id ?? '')
+
+    return (queryClient.getQueryData(query.queryKey) ??
+      (await queryClient.fetchQuery(query))) as Movie
+  }
 
 function MovieDetails() {
-  const {id = ''} = useParams()
-  const {data: movie, isLoading, isError, isSuccess} = useMovie(id)
+  const movie = useLoaderData() as Awaited<
+    ReturnType<ReturnType<typeof movieDetailsLoader>>
+  >
 
-  if (isLoading) {
-    return <p>Loading movie...</p>
-  }
-
-  if (isError) {
-    return <p>Unable to load movie</p>
-  }
-
-  if (isSuccess) {
-    return (
-      <div>
-        <Link to="/movies" className="mb-3 inline-block py-2 underline">
-          &larr; Back to all movies
-        </Link>
-        <h1 className="text-3xl">{movie.title}</h1>
-        <p className="mt-3">by {movie.director}</p>
-        <p className="mt-3">Release date: {formatDate(movie.release_date)}</p>
-        <div className="my-4 border border-dotted border-amber-500 p-3">
-          <h2 className="mb-3 font-bold">Summary</h2>
-          <p>{movie.opening_crawl}</p>
-        </div>
+  return (
+    <div>
+      <Link to="/movies" className="mb-3 inline-block py-2 underline">
+        &larr; Back to all movies
+      </Link>
+      <h1 className="text-3xl">{movie.title}</h1>
+      <p className="mt-3">by {movie.director}</p>
+      <p className="mt-3">Release date: {formatDate(movie.release_date)}</p>
+      <div className="my-4 border border-dotted border-amber-500 p-3">
+        <h2 className="mb-3 font-bold">Summary</h2>
+        <p>{movie.opening_crawl}</p>
       </div>
-    )
-  }
-
-  return null
+    </div>
+  )
 }
 
 export default MovieDetails
